2351e66c582c5a0cfe0f3f8fbfed5ac4aee1b096
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-01 16:33:50 +0300

    Include leaflet.css on index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import 'leaflet/dist/leaflet.css';
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
