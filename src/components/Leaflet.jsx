20524c95af797d0b2a8f4e871e7c32da5ab9599a
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-01 17:26:21 +0300

    Store customMarker in an external file, refactor Leaflet, import VesselTracks

--- src/components/Leaflet.jsx
+++ src/components/Leaflet.jsx
@@ -1,30 +1,19 @@
 import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import { Map, TileLayer } from 'react-leaflet';
+import VesselTracks from './VesselTracks';
 
 const position = [51.505, -0.09];
-
-const customMarker = new L.Icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png',
-    iconSize: [25, 41],
-    iconAnchor: [10, 41],
-    popupAnchor: [2, -40]
-});
+const mapStyle = { height: '100vh' };
 
 const Leaflet = () => {
     return (
-        <Map center={position} zoom={13} style={{ height: '100vh' }}>
+        <Map center={position} zoom={3} style={mapStyle}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Marker position={position} icon={customMarker}>
-                <Popup>
-                    A pretty CSS3 popup.
-                    <br />
-                    Easily customizable.
-                </Popup>
-            </Marker>
+
+            <VesselTracks />
         </Map>
     );
 };
