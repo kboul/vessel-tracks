91b138800e74c55671393f650a3d6bdbb8d7af69
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-01 18:08:36 +0300

    Bind popup with vessel track metadata on VesselTracks component

--- src/components/VesselTracks.jsx
+++ src/components/VesselTracks.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import L from 'leaflet';
 import PropTypes from 'prop-types';
 import { withLeaflet } from 'react-leaflet';
 import useVesselTracks from '../hooks/useVesselTracks';
 import { customMarker } from '../constants/customMarker';
+import PopupContent from './PopupContent';
 
 const VesselTracks = ({ leaflet: { map } }) => {
     const [vesselTracks] = useVesselTracks();
@@ -11,7 +14,14 @@ const VesselTracks = ({ leaflet: { map } }) => {
     vesselTracks.forEach(track => {
         const lat = parseFloat(track.LAT);
         const lon = parseFloat(track.LON);
-        L.marker([lat, lon], { icon: customMarker }).addTo(map);
+
+        L.marker([lat, lon], { icon: customMarker })
+            .addTo(map)
+            .bindPopup(
+                ReactDOMServer.renderToString(
+                    <PopupContent vesselTrack={track} />
+                )
+            );
     });
 
     return null;
