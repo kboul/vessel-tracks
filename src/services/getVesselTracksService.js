69d0ba137425c5019ccc66bd5514027a00d84cf2
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-01 17:07:39 +0300

    Create getVesselTracksService

---
+++ src/services/getVesselTracksService.js
@@ -0,0 +1,13 @@
+import httpService from './httpService';
+
+export const getVesselTracksService = async () => {
+    try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const endpoint = `${apiKey}/period:daily/days:3/mmsi:241486000/protocol:jsono`;
+        const response = await httpService.get(endpoint);
+        const { data } = response;
+        return data;
+    } catch (error) {
+        console.log('There was an error while getting the earthquakes', error);
+    }
+};
