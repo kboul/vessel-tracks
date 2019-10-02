981a00836131b3727f54719a5aa2244dc4cf9ff3
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-02 12:35:26 +0300

    Use tilelayers constant on Leaflet to switch between different tile prividers

--- src/components/Leaflet.jsx
+++ src/components/Leaflet.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, LayersControl } from 'react-leaflet';
 import VesselTracks from './VesselTracks';
+import tilelayers from '../constants/tilelayers';
+import { idGenerator } from '../utils/idGenerator';
 
 const position = [51.505, -0.09];
 const mapStyle = { height: '100vh' };
@@ -12,6 +14,16 @@ const Leaflet = () => {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
+            <LayersControl position="topright">
+                {tilelayers.map(({ name, attribution, url, checked }) => (
+                    <LayersControl.BaseLayer
+                        key={idGenerator()}
+                        name={name}
+                        checked={checked}>
+                        <TileLayer attribution={attribution} url={url} />
+                    </LayersControl.BaseLayer>
+                ))}
+            </LayersControl>
 
             <VesselTracks />
         </Map>
