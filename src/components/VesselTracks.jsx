5cd2a2a08ba5dc1794ad1e8a6caa7d1c950b3255
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-02 11:59:22 +0300

    Add marker clustering to VesselTracks component

--- src/components/VesselTracks.jsx
+++ src/components/VesselTracks.jsx
@@ -1,36 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import L from 'leaflet';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'leaflet.markercluster/dist/leaflet.markercluster';
 import PropTypes from 'prop-types';
 import { withLeaflet } from 'react-leaflet';
+import PopupContent from './PopupContent';
 import Spinner from './Spinner';
 import useVesselTracks from '../hooks/useVesselTracks';
 import { customMarker } from '../constants/customMarker';
-import PopupContent from './PopupContent';
 
 const fg = L.featureGroup();
+const mcg = L.markerClusterGroup();
 
 const VesselTracks = ({ leaflet: { map } }) => {
     const [vesselTracks, loading] = useVesselTracks();
-    // console.log(vesselTracks);
 
-    vesselTracks.forEach((vesselTrack, id) => {
-        const lat = parseFloat(vesselTrack.LAT);
-        const lon = parseFloat(vesselTrack.LON);
+    useEffect(() => {
+        vesselTracks.forEach((vesselTrack, id) => {
+            const lat = parseFloat(vesselTrack.LAT);
+            const lon = parseFloat(vesselTrack.LON);
 
-        L.marker([lat, lon], { icon: customMarker })
-            .addTo(fg)
-            .bindPopup(
-                ReactDOMServer.renderToString(
-                    <PopupContent vesselTrack={vesselTrack} />
-                )
-            );
+            L.marker(new L.LatLng(lat, lon), {
+                icon: customMarker
+            })
+                .addTo(fg)
+                .addTo(mcg)
+                .bindPopup(
+                    ReactDOMServer.renderToString(
+                        <PopupContent vesselTrack={vesselTrack} />
+                    )
+                );
 
-        if (vesselTracks.length - 1 === id) {
-            fg.addTo(map);
-            map.fitBounds(fg.getBounds());
-        }
-    });
+            if (vesselTracks.length - 1 === id) {
+                console.log(vesselTracks);
+                // add feature group to the map
+                fg.addTo(map);
+                // fit the map bounds to the markers relative coords
+                map.fitBounds(fg.getBounds());
+                // add the marker cluster group to the map
+                map.addLayer(mcg);
+            }
+        });
+    }, [vesselTracks]);
 
     if (loading) return <Spinner />;
 
