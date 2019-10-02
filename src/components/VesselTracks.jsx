240d6a615144432537d8ca1e2641ccba831e6107
kostas <konstantinos.voulgaridis@aspisid.com>
2019-10-02 14:57:22 +0300

    Import AnimateVesselTrack on VesselTracks to perform vessel track animation with two buttons as layer controls

--- src/components/VesselTracks.jsx
+++ src/components/VesselTracks.jsx
@@ -2,20 +2,25 @@ import React, { useEffect } from 'react';
 import ReactDOMServer from 'react-dom/server';
 import L from 'leaflet';
 import 'leaflet.markercluster/dist/leaflet.markercluster';
+import 'leaflet.animatedmarker/src/AnimatedMarker';
 import PropTypes from 'prop-types';
 import { withLeaflet } from 'react-leaflet';
 import PopupContent from './PopupContent';
 import Spinner from './Spinner';
+import AnimateVesselTrack from './AnimateVesselTrack';
 import useVesselTracks from '../hooks/useVesselTracks';
 import { customMarker } from '../constants/customMarker';
 
 const mcg = L.markerClusterGroup();
+const latlngs = [];
 
 const VesselTracks = ({ leaflet: { map } }) => {
     const [vesselTracks, loading] = useVesselTracks();
 
     useEffect(() => {
-        vesselTracks.forEach((vesselTrack, id) => {
+        console.log(vesselTracks);
+
+        vesselTracks.forEach(vesselTrack => {
             const lat = parseFloat(vesselTrack.LAT);
             const lon = parseFloat(vesselTrack.LON);
 
@@ -29,19 +34,18 @@ const VesselTracks = ({ leaflet: { map } }) => {
                     )
                 );
 
-            if (vesselTracks.length - 1 === id) {
-                console.log(vesselTracks);
-                // fit the map bounds to the mcg relative coords
-                map.fitBounds(mcg.getBounds());
-                // add the marker cluster group to the map
-                map.addLayer(mcg);
-            }
+            latlngs.push([lat, lon]);
+
+            // fit the map bounds to the mcg relative coords
+            map.fitBounds(mcg.getBounds());
+            // add the marker cluster group to the map
+            map.addLayer(mcg);
         });
     }, [vesselTracks, map]);
 
     if (loading) return <Spinner />;
 
-    return null;
+    return <AnimateVesselTrack latlngs={latlngs} />;
 };
 
 VesselTracks.propTypes = {
