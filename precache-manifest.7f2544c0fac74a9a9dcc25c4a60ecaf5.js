aa38e763fcf8b91f1490e674b20f334d9bbe580f
kboul <kos.voulgaridis@gmail.com>
2019-10-02 15:10:25 +0300

    Updates

---
+++ precache-manifest.7f2544c0fac74a9a9dcc25c4a60ecaf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac5463fe52aadaf69712d2f0ab2a791",
+    "url": "/react-daily-vessel-tracks/index.html"
+  },
+  {
+    "revision": "a5b251ad1c07bff7326d",
+    "url": "/react-daily-vessel-tracks/static/css/2.05e2f46e.chunk.css"
+  },
+  {
+    "revision": "a39701db957b8af59484",
+    "url": "/react-daily-vessel-tracks/static/css/main.4bd06e1a.chunk.css"
+  },
+  {
+    "revision": "a5b251ad1c07bff7326d",
+    "url": "/react-daily-vessel-tracks/static/js/2.98fefa57.chunk.js"
+  },
+  {
+    "revision": "a39701db957b8af59484",
+    "url": "/react-daily-vessel-tracks/static/js/main.8950e10c.chunk.js"
+  },
+  {
+    "revision": "c38b80d1dd02f80935ac",
+    "url": "/react-daily-vessel-tracks/static/js/runtime-main.63aa378b.js"
+  }
+]);
